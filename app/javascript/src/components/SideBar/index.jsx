1269721094fb37f5fb9ba614f63a8ec89a2a2763
Joseph Jojy Chirayath <joseph.chirayath@bigbinary.com>
2021-11-26 15:50:09 +0530

    Added Menubar

--- app/javascript/src/components/SideBar/index.jsx
+++ app/javascript/src/components/SideBar/index.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Search, Plus } from "@bigbinary/neeto-icons";
+import { Typography } from "@bigbinary/neetoui/v2";
+import { MenuBar } from "@bigbinary/neetoui/v2/layouts";
 
 const SideBar = () => {
-  return <div></div>;
+  const [isSearchCollapsed, setIsSearchCollapsed] = useState(true);
+
+  return (
+    <div className="flex">
+      <MenuBar showMenu={true} title="Articles">
+        <MenuBar.Block label="All" count={13} active />
+        <MenuBar.Block label="Draft" count={2} />
+        <MenuBar.Block label="Published" count={7} />
+
+        <MenuBar.SubTitle
+          iconProps={[
+            {
+              icon: Search,
+              onClick: () => setIsSearchCollapsed(!isSearchCollapsed),
+            },
+            {
+              icon: Plus,
+            },
+          ]}
+        >
+          <Typography
+            component="h4"
+            style="h5"
+            textTransform="uppercase"
+            weight="bold"
+          >
+            Categories
+          </Typography>
+        </MenuBar.SubTitle>
+        <MenuBar.Search
+          collapse={isSearchCollapsed}
+          onCollapse={() => setIsSearchCollapsed(true)}
+        />
+        <MenuBar.Block label="Getting Started" count={80} />
+        <MenuBar.Block label="Apps Integration" count={40} />
+        <MenuBar.Block label="Misc" count={20} />
+      </MenuBar>
+    </div>
+  );
 };
 
 export default SideBar;
