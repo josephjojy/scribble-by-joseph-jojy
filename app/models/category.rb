a837462eca458ce779ce2a5aa9e1e7e4fb84f704
Joseph Jojy Chirayath <joseph.chirayath@bigbinary.com>
2021-12-06 16:51:11 +0530

    Add Position to Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,13 @@
 class Category < ApplicationRecord
   has_many :articles
 
+  after_create :set_position
+
   validates :name, presence: true
+
+  private
+
+    def set_position
+      update(position: self.id)
+    end
 end
